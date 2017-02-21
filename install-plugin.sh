51ff44c0f6782c42df6aa9529f3cefb74a1c3444
binlee <bin.lee0@gmail.com>
2017-02-21 23:07:25 +0800

    install emmet, atom-beautify

--- install-plugin.sh
+++ install-plugin.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-ATOM_PACKAGE_PATH=/c/Users/Administrator/.atom/packages
+ATOM_PACKAGE_PATH=${HOME}/.atom/packages
 declare -A gitUrls
 
-gitUrls=(["atom-beautify"]=https://github.com/Glavin001/atom-beautify.git)
+gitUrls=(["atom-beautify"]="https://github.com/Glavin001/atom-beautify.git"
+	["emmet-atom"]="https://github.com/emmetio/emmet-atom.git"
+)
 cd $ATOM_PACKAGE_PATH
 for k in "${!gitUrls[@]}"
 do
   #echo "${k} -> ${gitUrls[${k}]}"
-  git clone --depth=1 https://github.com/Glavin001/atom-beautify.git
+  git clone --depth=1 ${gitUrls[${k}]} 
   cd ${k}
-  npm install
+  npm install --verbose
+  cd -
 done
